2f509f7a020be76c0c21060a7e026f71e5e154f0
Pedro Antonhyonhi Silva Costa <antonhyonhi@gmail.com>
2021-11-11 16:21:33 -0300

    Correçoes no 'main'

--- main.py
+++ main.py
@@ -30,11 +30,11 @@ def type_following(first_word: str) -> str:
     print(f'proximas palavras: {following_list}')
     logging.info('Digite a palavra seguinte:')
     following_word = input()
-    data_base.count_following(first_word, following_word)
+    data_base.count_word(following_word)
     return following_word
 
-
-def type_word() -> str:
+# Retorna a palavra contada e se o programa deve sugerir a seguinte
+def type_word() -> Tuple[bool, str]:
     data_base = DataBase(FILE_PATH, DICT_PATH)
     logging.info('Digite a palavra desejada:')
     typed_word = input()
@@ -49,10 +49,10 @@ def type_word() -> str:
         if correct_word == typed_word:
             data_base.insert_word(correct_word)
         data_base.count_word(correct_word)
-        return correct_word
+        return (True, correct_word)
     else:
         data_base.count_word(typed_word)
-        return typed_word
+        return (False, typed_word)
     
 def print_freq_words():
     data_base = DataBase(FILE_PATH, DICT_PATH)
@@ -60,7 +60,7 @@ def print_freq_words():
 
 def print_freq_after():
     data_base = DataBase(FILE_PATH, DICT_PATH)
-    logging.info('Digite a palavra correta:')
+    logging.info('Digite a palavra a ser consultada:')
     first = input()
     print(data_base.following_str(first))
 
@@ -74,8 +74,9 @@ while entry != 'e':
     if(entry == 'i'):
         insert_words()
     elif(entry == 'd'):
-        correct_first = type_word()
-        type_following(correct_first)
+        (shoud_type_following, correct_first) = type_word()
+        if shoud_type_following:
+            type_following(correct_first)
     elif(entry == 'f'):
         print_freq_words()
     elif(entry == 'p'):
